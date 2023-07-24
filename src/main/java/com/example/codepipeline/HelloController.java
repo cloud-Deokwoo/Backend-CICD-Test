d3d04859c48865dbbcfcef293dd4fe5f1623a711
kdw335552 <kdw335552@nate.com>
2023-07-24 12:20:54 +0900

    jenkins test1220

--- src/main/java/com/example/codepipeline/HelloController.java
+++ src/main/java/com/example/codepipeline/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String root() {
-        return "Hi This page BEFORE MODIFIED2 입니다.";
+        return "Hi This page BEFORE MODIFIED2222222 입니다.";
     }
 }
